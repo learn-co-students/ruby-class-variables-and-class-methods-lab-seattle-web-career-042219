234ebe6a88c204684cf50d30fe2c14b0ceba0a79
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-04-29 10:53:54 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+	@@count = 0
+	@@artists = []
+	@@genres = []
+
+	attr_accessor :name, :artist, :genre 
+
+
+	def initialize(name, artist,genre)
+		@name, @artist, @genre = name, artist,genre
+		@@count += 1
+		@@artists << artist
+		@@genres << genre
+	end
+
+	def self.count
+		@@count
+	end
+
+	def self.artists
+		@@artists.uniq
+	end
+
+	def self.genres
+		@@genres.uniq
+	end
+
+	def self.genre_count
+		counts = Hash.new(0)
+		@@genres.each do |name|
+		    counts[name] += 1
+	  	end	
+		counts
+	end
+
+	def self.artist_count
+		counts = Hash.new(0)
+		@@artists.each do |artist|
+			counts[artist] += 1
+		end
+		counts
+	end
+
+
+end
